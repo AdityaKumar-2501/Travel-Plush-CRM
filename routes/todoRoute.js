d6ba8c44955c671455fc44d69bed2e4acc415f20
Rahul <adityakashyap2501@gmail.com>
2024-07-25 16:20:35 +0530

    testing

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -3,4 +3,11 @@ const router = express.Router();
 const verifyToken = require("../middlewares/authentication");
 const { testRouter, getAllTodos, createTodo, updateTodo, deleteTodo, getReminder } = require('../controllers/todoController');
 
+router.get('/', verifyToken, testRouter)
+router.get('/get',verifyToken, getAllTodos)
+router.post('/post', verifyToken,createTodo)
+router.put('/update',verifyToken, updateTodo)
+router.delete('/delete',verifyToken, deleteTodo)
+router.get('/reminder',verifyToken, getReminder);
+
 module.exports = router;
